6ee5874e5964043489b297f332266a8a363d031d
kc1r74p <7422353+kc1r74p@users.noreply.github.com>
2020-07-13 23:57:46 +0200

    Adjusted metrics

--- src/index.js
+++ src/index.js
@@ -7,40 +7,46 @@ const vRef = 230; // Volt
 const measureDeviceHost = 'http://192.168.178.62/current?adc=';
 const channels = {
     0: {
-        name: 'Treppenhaus Keller',      
+        name: 'Treppenhaus Keller',
         offset: -14
     },
     1: {
-        name: 'L1',      
-        offset: -14        
+        name: 'L1',
+        offset: -14
     },
     2: {
-        name: 'Heizung',      
-        offset: -14        
+        name: 'Heizung',
+        offset: -14
     },
     3: {
-        name: 'L3',      
-        offset: -14        
+        name: 'L3',
+        offset: -14
     },
     4: {
-        name: 'L2',      
-        offset: -14        
+        name: 'L2',
+        offset: -14
     },
     5: {
-        name: 'Server + WAN',      
-        offset: -14        
+        name: 'Server + WAN',
+        offset: -14
     }
 };
 
 const server = http.createServer(async function (request, response) {
     const lines = [];
-    lines.push('# HELP nrg_current_measure The current * 1000 in A of a certain channel');
-    lines.push('# TYPE nrg_current_measure gauge');
+    lines.push('# HELP nrg_current_measure_raw The current * 1000 in A of a certain channel');
+    lines.push('# TYPE nrg_current_measure_raw gauge');
+    lines.push('# HELP nrg_power_measure The power in Watt of a certain channel');
+    lines.push('# TYPE nrg_power_measure gauge');
 
-    //for active channels:
-    for (const [channel, cfg] of Object.entries(channels)) {
-        const response = await got(measureDeviceHost + channel).json();
-        lines.push(`nrg_current_measure{ channel = "${channel}", name = "${cfg.name}" } ${(response.return_value * (vRef / 1000) + cfg.offset).toFixed(4)}`);
+    try {
+        for (const [channel, cfg] of Object.entries(channels)) {
+            const response = await got(measureDeviceHost + channel).json();
+            lines.push(`nrg_current_measure_raw{ channel = "${channel}", name = "${cfg.name}" } ${(response.return_value).toFixed(4)}`);
+            lines.push(`nrg_power_measure{ channel = "${channel}", name = "${cfg.name}" } ${(response.return_value * (vRef / 1000) + cfg.offset).toFixed(4)}`);
+        }
+    } catch (e) {
+        console.log(e);
     }
 
     response.writeHead(200, { 'Content-Type': 'text/plain' })
